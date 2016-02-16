ce27b971cb84b795dd485ddb60d085c838375e52
MTBellAtWhitworth <mbell@whitworth.edu>
2016-02-16 09:58:27 -0800

    Original, written to illustrate how to post to GitHub.

---
+++ HelloGitHub/HelloGitHub/Catface.cpp
@@ -0,0 +1,13 @@
+//Author: Matthew Bell
+//Purpose: Illustrate GitHub by posting a species
+//         of hello world....a weird one!
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Why, hello, Catface!";
+	return 0;
+}
